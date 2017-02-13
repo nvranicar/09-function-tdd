cf28e973367f21e1cbb61eaa607d8ef3b48e6ac0
Nathan Vranicar <nathan@Nathans-MacBook-Pro-4.local>
2017-02-13 16:34:13 -0600

    finished blackjack

--- app/blackjack/select-action.js
+++ app/blackjack/select-action.js
@@ -1,4 +1,13 @@
 import shouldSplit from './should-split';
 import shouldHit from './should-hit';
 
-export default function () {}
+export default function selectAction(card1, card2) {
+  if (shouldSplit(card1, card2) === true) {
+    return 'split';
+  }
+  if (shouldHit(card1, card2) === true) {
+    return 'hit';
+  }
+
+  return 'stay';
+}
