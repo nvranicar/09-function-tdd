3d50d1780aea15de3249d78d6cf7b982da8f1bf9
Nathan Vranicar <nathan@Nathans-MacBook-Pro-4.local>
2017-02-13 17:51:08 -0600

    added code for alive neighbors

--- app/conway/alive-neighbors.js
+++ app/conway/alive-neighbors.js
@@ -1,1 +1,11 @@
-export default function () {}
+export default function aliveNeighbors(arr) {
+  let sum = 0;
+
+  for (let i = 0; i < arr.length; i += 0) {
+    if (arr[i] === true) {
+      sum += 1;
+    }
+  }
+
+  return sum;
+}
