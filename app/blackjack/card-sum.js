057f1af02aaee2b287d8d9cd9aa81410991f529c
Nathan Vranicar <nathan@Nathans-MacBook-Pro-4.local>
2017-02-13 15:29:07 -0600

    imported sum function from class

--- app/blackjack/card-sum.js
+++ app/blackjack/card-sum.js
@@ -1,3 +1,12 @@
 import cardValue from './card-value';
 
-export default function () {}
+export default function sum(arr) {
+  const length = arr.length;
+  let sumValue = 0;
+
+  for (let i = 0; i < length; i += 1) {
+    sumValue += arr[i];
+  }
+
+  return sumValue;
+}
